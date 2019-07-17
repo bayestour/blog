418275fb1b5e62fd8ca4c79bdcc4f21aa89e9dea
Sungbin Lim <leo.brain@kakaobrain.com>
2019-07-17 16:19:13 +0900

    DP 내용 commit

--- code/dirichlet_process.py
+++ code/dirichlet_process.py
@@ -1,4 +1,18 @@
 import numpy as np
-import scipy as sc
+import scipy as sp
 import pandas as pd
+import pymc3 as pm
+import seaborn
 
+blue, *_ = seaborn.color_palette()
+
+N = 20
+K = 30
+alpha = 2.0
+P0 = sp.stats.norm
+beta = sp.stats.beta.rvs(1, alpha, size=(N,K))
+w = np.empty_like(beta)
+w[:, 0] = beta[:, 0]
+w[:, 1:] = beta[:, 1:] * (1-beta[:, :-1]).cumprod(axis=1) # w[i,j] = beta[i,j] * \prod_{l=0}^{j-1} (1-beta[i,l])
+
+omega =  
