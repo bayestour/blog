096092927976838abdb69b05a8bae4107b4ec10b
Sungbin Lim <leo.brain@kakaobrain.com>
2019-07-19 14:34:17 +0900

    dp-update

--- code/dirichlet_process.py
+++ code/dirichlet_process.py
@@ -3,11 +3,12 @@ import scipy as sp
 import pandas as pd
 import pymc3 as pm
 import seaborn
+import matplotlib.pyplot as plt
 
 blue, *_ = seaborn.color_palette()
 
-N = 20
-K = 30
+N = 20  # number of data points
+K = 30  # number of clusters
 alpha = 2.0
 P0 = sp.stats.norm
 beta = sp.stats.beta.rvs(1, alpha, size=(N,K))
@@ -15,4 +16,8 @@ w = np.empty_like(beta)
 w[:, 0] = beta[:, 0]
 w[:, 1:] = beta[:, 1:] * (1-beta[:, :-1]).cumprod(axis=1) # w[i,j] = beta[i,j] * \prod_{l=0}^{j-1} (1-beta[i,l])
 
-omega =  
+omega = P0.rvs(size=(N,K))
+
+x_plot = np.linspace(-3, 3, 300)
+
+sample_cdfs = (w[...,np.newaxis]) * np.less
